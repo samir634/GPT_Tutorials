fe531ad24a2bea6bf0f3afb7ac371c6fd7dcde3e
Samir Desai <dewbuk@gmail.com>
2023-08-08 17:56:15 -0400

    webcrawler tutorial

---
+++ WebCrawler/WebCrawler/parser_1.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+
+# Define root domain to crawl
+domain = "openai.com"
+full_url = "https://openai.com/"
+
+
+def remove_newlines(serie):
+    serie = serie.str.replace('\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('  ', ' ')
+    serie = serie.str.replace('  ', ' ')
+    return serie
+
+# Create a list to store the text files
+texts=[]
+
+# Get all the text files in the text directory
+for file in os.listdir("text/" + domain + "/"):
+
+    # Open the file and read the text
+    with open("text/" + domain + "/" + file, "r", encoding="UTF-8") as f:
+        text = f.read()
+
+        # Omit the first 11 lines and the last 4 lines, then replace -, _, and #update with spaces.
+        texts.append((file[11:-4].replace('-',' ').replace('_', ' ').replace('#update',''), text))
+
+# Create a dataframe from the list of texts
+df = pd.DataFrame(texts, columns = ['fname', 'text'])
+
+# Set the text column to be the raw text with the newlines removed
+df['text'] = df.fname + ". " + remove_newlines(df.text)
+df.to_csv('processed/scraped.csv')
+df.head()
